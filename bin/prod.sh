85cc2a00f9292f52d0c925f0c755e0145b291a99
blacktea <blacktea1988@gmail.com>
2018-08-07 14:21:07 +0800

    启动脚本

---
+++ bin/prod.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+PORT=8080
+NODE_ENV=production
+
+pid="`netstat -tlanp | grep LISTEN | grep ${PORT} | sed -r 's/^.+ ([0-9]+)\/.+$/\1/'`"
+
+if [ $pid ]; then
+  kill $pid
+  echo "process has been stoped. [$pid]"
+else
+  echo "process is not running."
+fi
+
+# 切换到当前代码根目录
+cd "$(dirname $0)/.."
+# 创建nohup目志输出目录
+mkdir -p ./nohup.out.d
+# 当前日期
+current=`date +%F`
+
+nohup npm start --brandfootprint </dev/null &>> ./nohup.out.d/${current}-p${PORT} &
+
+echo "process has been started."
